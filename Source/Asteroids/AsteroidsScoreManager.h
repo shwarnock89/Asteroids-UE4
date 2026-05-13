894715110d936393a037f33818ab7c1a61a88443
Warnock <swarnock@respawn.com>
2026-05-13 15:27:32 -0500

    Removing Messenger class

---
+++ Source/Asteroids/AsteroidsScoreManager.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AsteroidsScoreManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTEROIDS_API UAsteroidsScoreManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
